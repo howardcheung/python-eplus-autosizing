bd5038e8feb12e9c075ce770e7f81227010f2bc6
howardcheung <howard.at@gmail.com>
2017-11-13 12:22:08 +0800

    Define the I/O of the new idf function

--- src/hardsizeeplus.py
+++ src/hardsizeeplus.py
@@ -3,7 +3,7 @@
     This script reads the .eio file of an eplus simulation result and
     replace the autosize inputs in the eplus file with the actual value
     in the eio file. Only applicable for the idf files in the ../dat/
-    folder.
+    folder (Large office model from PNNL in 2013)
 
     Name: Howard Cheung, Jiefang Gu
     Date: 2017/11/13
@@ -55,7 +55,7 @@ def main(old_idf_file: str, eio_file: str, new_idf_file: str):
     # output the new idf file
 
 
-def read_file(filepath: str) -> str:
+def read_file(filepath: str):
     """
         Read the text file in filepath and return the corresponding
         string in UTF-8 coding
@@ -71,7 +71,34 @@ def read_file(filepath: str) -> str:
 
     return txt
 
+
+def create_new_idf(old_idf_txt: str, eio_txt: str):
+    """
+        Create the new idf file by copying the values in the eio to
+        replace the autosized inputs in the original idf file.
+
+        Inputs:
+        ==========
+        old_idf_txt: strings
+            strings of the old idf file in UTF-coding
+
+        eio_txt: strings
+            strings of the eio file in UTF-coding
+    """
+
+    # create new string object with autosizing information of Sizing:System
+    # objects
+
+    # modify the string object with sizing information of
+    # AirTerminal:SingleDuct:Uncontrolled object
+
+    # modify the string object for the rest of the components
+
+    pass
+
+
 # running the script file
 if __name__ == '__main__':
 
+    # runnning the main function with inputs from the command line/ terminal
     main(sys.argv[1], sys.argv[2], sys.argv[3])
