34630265f032504a0ca64d0ed222254f31839d9e
howardcheung <howard.at@gmail.com>
2017-11-13 11:19:18 +0800

    Create the template

---
+++ src/hardsizeeplus.py
@@ -0,0 +1,39 @@
+#!usr/bin/python3
+"""
+    This script reads the .eio file of an eplus simulation result and
+    replace the autosize inputs in the eplus file with the actual value
+    in the eio file. Only applicable for the idf files in the ../dat/
+    folder.
+
+    Name: Howard Cheung
+    Date: 2017/11/13
+"""
+
+# internal libraries
+
+
+# external libraries
+
+
+# user-defined libraries
+
+
+# global variables
+
+
+# uesr-defined classes
+
+
+# user-defined functions
+def main():
+    """
+        Main function to be excuted
+    """
+
+    pass
+
+
+# running the script file
+if __name__ == '__main__':
+
+    main()
