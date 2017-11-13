b860e7e21248e08da6d6c2af1317c347a64f96a2
howardcheung <howard.at@gmail.com>
2017-11-13 15:58:26 +0800

    Copying the design air flow rate of AirLoopHVAC objects

--- src/hardsize_comp.py
+++ src/hardsize_comp.py
@@ -72,18 +72,51 @@ def copy_sizing_system_info(old_idf_txt: str, eio_txt: str):
             obj_txt = upper_txt[obj_txt_re.span()[0]:obj_txt_re.span()[1]]
             if sys_size_data.loc[ind, ' Load Type'] == ' Cooling':
                 # find the location of the particular string to be replaced
+                # for the cooling capacity
                 txt_re = re.compile(
                     b''.join([b'.+\n']*30+[b'\s*AUTOSIZE'])
-                ).search(obj_txt)
+                ).search(obj_txt)  # find the 30th field in Sizing:System
             elif sys_size_data.loc[ind, ' Load Type'] == ' Heating':
                 # find the location of the particular string to be replaced
+                # for the heating capacity
                 txt_re = re.compile(
                     b''.join([b'.+\n']*34+[b'\s*AUTOSIZE'])
-                ).search(obj_txt)
+                ).search(obj_txt)  # find the 34th field in Sizing:System
             if txt_re is not None:
                 # replace the string
                 new_txt = str(
                     sys_size_data.loc[ind, ' User Design Capacity']
+                ).encode().join([
+                    new_txt[0:obj_txt_re.span()[0]+txt_re.span()[1]-8],
+                    new_txt[obj_txt_re.span()[0]+txt_re.span()[1]:]
+                ])
+
+    # copy the design airflow from the AirLoopHVAC objects
+    for ind in comp_size_data.index:
+        # use upper text only to search for consistent
+        upper_txt = new_txt.upper()
+        # find the location of the object
+        obj_txt_re = re.compile(b''.join([
+            b'SIZING:SYSTEM,\s*\n\s*',
+            comp_size_data.loc[
+                ind, ' Component Name'
+            ].encode().replace(b'_', b'\_'), b'[^;]+'
+        ])).search(upper_txt)
+        if obj_txt_re is not None and \
+                comp_size_data.loc[
+                    ind, ' Component Type'
+                ] == ' AirLoopHVAC' and comp_size_data.loc[
+                    ind, ' Input Field Description'
+                ] == ' Design Supply Air Flow Rate [m3/s]':
+            # to bypass error
+            obj_txt = upper_txt[obj_txt_re.span()[0]:obj_txt_re.span()[1]]
+            txt_re = re.compile(
+                b''.join([b'.+\n']*3+[b'\s*AUTOSIZE'])
+            ).search(obj_txt)  # find the 3rd field in Sizing:System
+            if txt_re is not None:
+                # replace the string
+                new_txt = str(
+                    comp_size_data.loc[ind, ' Value']
                 ).encode().join([
                     new_txt[0:obj_txt_re.span()[0]+txt_re.span()[1]-8],
                     new_txt[obj_txt_re.span()[0]+txt_re.span()[1]:]
@@ -105,10 +138,14 @@ if __name__ == '__main__':
     # check the correctness of the copying for the system:sizing
     assert copy_sizing_system_info(testidf, testeio).find(
         b'75214.72,                !- Cooling Design Capacity {W}'
-    )
+    ) > 0  # for cooling capacity
     assert copy_sizing_system_info(testidf, testeio).find(
         b'45424.36,                !- Heating Design Capacity {W}'
-    )
+    ) > 0 # for heating capacity
+    assert copy_sizing_system_info(testidf, testeio).find(
+        b'4.94579,                !- Design Outdoor Air Flow Rate {m3/s}'
+    ) > 0  # for airflow
+    print(copy_sizing_system_info(testidf, testeio).decode())
 
     # print statement
     print('File hard_size_comp.py is OK!')
