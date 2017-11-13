c3af2994e77cd33128a5440d6d5fb1d18621bb29
howardcheung <howard.at@gmail.com>
2017-11-13 16:46:18 +0800

    Test the running of an eplus file

--- src/test.sh
+++ src/test.sh
@@ -10,3 +10,14 @@ python3 hardsizeeplus.py ../dat/ASHRAE90.1_OfficeLarge_STD2013_Miami.idf ../dat/
 
 # diff the file
 diff ../dat/ASHRAE90.1_OfficeLarge_STD2013_Miami.idf ../results/new.idf > ../results/idf_diff.txt
+
+# run the new idf
+energyplus -w ../dat/USA_FL_Tampa.Intl.AP.722110_TMY3.epw -p neweplus -r ../results/new.idf
+
+# copy all files with neweplus heading to the results directory
+for file in ./neweplus*; do 
+    mv "$file" ../results/
+done
+
+# compare eio files
+diff ../dat/eplustlb.csv ../results/neweplustbl.csv > ../results/tbl.csv.diff
